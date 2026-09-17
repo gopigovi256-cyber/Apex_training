165723e9332f8f8fd454a27c88666ebedd31cfc4
gopigovi256-cyber <gopigovi256@gmail.com>
2026-09-17 14:31:12 +0530

    Add files via upload

---
+++ arithmetic.py
@@ -0,0 +1,13 @@
+Python 3.15.0b3 (tags/v3.15.0b3:cf16a33, Jun 23 2026, 10:03:50) [MSC v.1951 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> a = float(input("Enter first number: "))
+... b = float(input("Enter second number: "))
+... 
+... print("Addition:", a + b)
+... print("Subtraction:", a - b)
+... print("Multiplication:", a * b)
+... 
+... if b != 0:
+...     print("Division:", a / b)
+... else:
+...     print("Division: Cannot divide by zero")
